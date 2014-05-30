9c047f0a5a2a5848741f2f38fc2b0d7c33af072d
Claros <naji.demolitionman@gmail.com>
2014-05-30 20:45:52 +0200

    7.41.1 - zuul-with-enums-v2

--- CommandWord.java
+++ CommandWord.java
@@ -1,4 +1,24 @@
 
 public enum CommandWord {
-	GO, QUIT, HELP, UNKNOWN, LOOK, EAT, BACK, TEST, TAKE, DROP, ITEMS;
+	GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), LOOK("look"), EAT("eat"), BACK("back"), TEST("test"), TAKE("take"), DROP("drop"), ITEMS("items");
+    
+    // The command string.
+    private String commandString;
+    
+    /**
+     * Initialise with the corresponding command word.
+     * @param commandWord The command string.
+     */
+    CommandWord(String commandString)
+    {
+        this.commandString = commandString;
+    }
+    
+    /**
+     * @return The command word as a string.
+     */
+    public String toString()
+    {
+        return commandString;
+    }
 }
